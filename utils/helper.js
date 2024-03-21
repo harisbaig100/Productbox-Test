849cf479afe9f665d8c911118f4d0e70c184090e
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-21 13:11:53 +0500

    code improvements

--- utils/helper.js
+++ utils/helper.js
@@ -18,7 +18,7 @@ module.exports = {
 	},
     getTitleFromUrl: function (address, sendTitle) {
         // Regular expression to extract the title from the HTML content of the URL
-        const regex = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
+        const regex = /<title>(.*?)<\/title>/;
 		// Ensure that "www." and "http://" are included in the URL to prevent invalid URLs
         const addWww = address.includes("www.") ? '' : 'www.';
         const addHttp = address.includes("http://") ? '' : 'http://';
@@ -26,7 +26,7 @@ module.exports = {
         if (url.includes(".com") || this.isValidURL(address)) {
 			// Using the URL string directly with http.get simplifies the request without custom options
 			// because we don't dynamically modify host, path, or port, nor include additional headers.
-
+           
             http.get(url, function (response) {
                 let data = '';
 				// if url is accessible get packet and match it to regex to get title
@@ -34,10 +34,14 @@ module.exports = {
                     data += chunk;
                 });
 
+                response.on('error', function(error) {
+                    console.error('Error in HTTP request:', error);
+                });
+
                 response.on('end', function () {
-                    const match = regex.exec(data);
-                    const title = match && match[2] ? match[2] : "Title Not Found";
-					// if match[2] is there send that as a title other wise show no title found
+                    const match = data.match(regex)
+                    const title = match && match[1] ? match[1] : "Title Not Found";
+					// if match[1] is there send that as a title other wise show no title found
                     sendTitle(address + ' - ' + (response.statusCode == 200 ? `"${title}"` : "Title Not Found"));
                 });
 			// return call back with error if get request having issues
